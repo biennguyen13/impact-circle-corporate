9bdb16f49b170e58cf094528e0d92aa3416f5da2
biennguyen <biennguyen131311@gmail.com>
2023-10-22 14:54:33 +0700

    Updates

---
+++ assets/index.9b844eb3.js
@@ -0,0 +1,1 @@
+import{d as n,r as s,o,c as a,u as c}from"./entry.5b601ef5.js";const u=n({__name:"index",setup(r){const{$axios:t}=c();return t.get("news").then(e=>e),s({search:"",options:null}),(e,p)=>(o(),a("div"))}});export{u as default};
