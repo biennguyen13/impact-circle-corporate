fbdede312764b36ebe91c5f3f6778c1127ca892c
biennguyen <biennguyen131311@gmail.com>
2023-10-22 14:23:33 +0700

    Updates

---
+++ _nuxt/index.fb8ba080.js
@@ -0,0 +1,1 @@
+import{d as n,r as s,o,c as a,u as c}from"./entry.a6e98b4c.js";const u=n({__name:"index",setup(r){const{$axios:t}=c();return t.get("news").then(e=>e),s({search:"",options:null}),(e,p)=>(o(),a("div"))}});export{u as default};
